8b798259daae80fb84b29d6816286f98b3fc2db3
赵文浩 <1050986935@qq.com>
2022-03-19 15:19:28 +0800

    KNN算法

---
+++ KNN算法案例.py
@@ -0,0 +1,71 @@
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+
+
+
+def knncls():
+    """
+    K-近邻预测用户签到位置
+    :return:
+    """
+    # 读取数据
+    data = pd.read_csv("C:/Users/10509/Desktop/AIStudy/FBlocation/train.csv")
+    # 处理数据
+    # 1.缩小数据，通过pandas查询数据筛选
+    data = data.query("x>1.0 & x<1.25 & y>2.5 & y<2.75")
+
+    # 2.处理时间
+    time_value = pd.to_datetime(data['time'], unit='s')
+    # 把日期格式转换成字典格式
+    time_value = pd.DatetimeIndex(time_value)
+
+    # 3.构造一些特征
+    data['day'] = time_value.day
+    data['hour'] = time_value.hour
+    data['weekday'] = time_value.weekday
+
+    # 4.删除原来的时间戳
+    data = data.drop(['time'], axis=1)
+
+    # 5.把签到数量少于n个目标位置删除
+    place_count = data.groupby('place_id').count()
+    tf = place_count[place_count.row_id>3].reset_index()
+    data = data[data['place_id'].isin(tf.place_id)]
+
+    # 6.取出数据中的特征值和目标值
+    y = data['place_id']
+    # data = data.drop(['place_id'], axis=1)
+    x = data.drop(['row_id'], axis=1)
+    print(x)
+
+    # 7.把数据分成训练集和测试集
+    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.25)
+
+    # 特征工程（标准化）对训练集和测试集的特征值进行归一化
+    std = StandardScaler()
+    X_train = std.fit_transform(X_train)
+    X_test = std.fit_transform(X_test)
+
+
+    # 训练数据
+    knn = KNeighborsClassifier(n_neighbors=5)
+
+    # fit, predice, score
+    knn.fit(X_train, y_train)
+    y_predict = knn.predict(X_test)
+    print("预测目标位置为", y_predict)
+
+    # 得出准确率
+    print("预测准确率", knn.score(X_test, y_test))
+
+
+
+    return None
+
+
+
+
+if __name__ == "__main__":
+    knncls()
---
+++ 鸢尾花KNN.py
@@ -0,0 +1,35 @@
+import pandas as pd
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+
+def iris_Knn():
+
+    # 加载数据集
+    iris = load_iris()
+    data = pd.DataFrame(iris.data)
+    X = data.iloc[:, 2:4]
+    y = iris.target
+
+    # 切分数据集
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
+
+    # 分类
+    knn_iris = KNeighborsClassifier(n_neighbors=5)
+    knn_iris.fit(X_train, y_train)
+    y_predict = knn_iris.predict(X_test)
+    print("预测目标位置为", y_predict)
+    print("预测准确率为", knn_iris.score(X_test, y_test))
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    iris_Knn()
