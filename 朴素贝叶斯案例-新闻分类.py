dc3bdeac0bb6e9d9bfc77f175333a3e255bcc923
赵文浩 <1050986935@qq.com>
2022-03-19 15:19:56 +0800

    朴素贝叶斯分类

---
+++ 朴素贝叶斯案例-新闻分类.py
@@ -0,0 +1,45 @@
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+
+
+
+def naviebayes():
+
+    """
+    朴素贝叶斯进行文本分类
+    :return:
+    """
+    news = fetch_20newsgroups(subset='all')
+    X = news.data
+    y = news.target
+    print(y)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
+
+    # 对数据进行特征抽取
+    news_tf = TfidfVectorizer()
+    # 以训练集当中的词的列表进行每篇文章重要性统计
+    X_train = news_tf.fit_transform(X_train)
+    # print(news_tf.get_feature_names())
+    X_test = news_tf.transform(X_test)
+
+    # 进行朴素贝叶斯算法的预测
+    mlt = MultinomialNB(alpha=1.0)
+    mlt.fit(X_train, y_train)
+    y_predict = mlt.predict(X_test)
+
+    print("预测得到的分类是：", y_predict)
+    print("准确率是：",mlt.score(X_test, y_test))
+
+
+    return None
+
+
+if __name__ == "__main__":
+    naviebayes()
+
+
+# [10  3 17 ...  3  1  7]
+# 预测得到的分类是： [11  2 10 ...  8  5  6]
+# 准确率是： 0.8406196943972836
